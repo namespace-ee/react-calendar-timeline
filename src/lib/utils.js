8bbacca0b5f2b9e1f3e577ae2eac7a6c15ed9c77
dbhaasis <david.haasis@hotmail.com>
2017-09-27 18:59:27 -0500

    Update utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -79,7 +79,7 @@ export function getParentPosition (element) {
 
   while (element) {
     if (!element.offsetParent && element.tagName === 'BODY' && element.scrollLeft === 0 && element.scrollTop === 0) {
-      element = document.scrollingElement || element;
+      element = document.scrollingElement || element
     }
     xPosition += (element.offsetLeft - (first ? 0 : element.scrollLeft) + element.clientLeft)
     yPosition += (element.offsetTop - (first ? 0 : element.scrollTop) + element.clientTop)
