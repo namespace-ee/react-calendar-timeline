7f36df5a17598d548319b79fff32680c26896cf9
Mark Shearman <kryptos@krypticsoftware.com>
2017-12-12 17:25:35 +0000

    Update TodayLine.js
    Used a property for the TodayLine time - to be used for a reference time rather than Now.

--- src/lib/lines/TodayLine.js
+++ src/lib/lines/TodayLine.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 
 export default class TodayLine extends Component {
   static propTypes = {
+    referenceTime: PropTypes.number.isRequired,
     canvasTimeStart: PropTypes.number.isRequired,
     canvasTimeEnd: PropTypes.number.isRequired,
     canvasWidth: PropTypes.number.isRequired,
@@ -13,13 +14,12 @@ export default class TodayLine extends Component {
   static defaultProps = {
   }
 
-  // TODO: should currentTime come from a prop? probably...?
   render () {
-    let currentTime = new Date().getTime()
+    let refTime = this.props.referenceTime;
 
-    if (currentTime > this.props.canvasTimeStart && currentTime < this.props.canvasTimeEnd) {
+    if (refTime > this.props.canvasTimeStart && refTime < this.props.canvasTimeEnd) {
       const ratio = this.props.canvasWidth / (this.props.canvasTimeEnd - this.props.canvasTimeStart)
-      const left = Math.round((currentTime - this.props.canvasTimeStart) * ratio)
+      const left = Math.round((refTime - this.props.canvasTimeStart) * ratio)
       const top = this.props.headerHeight
       const height = this.props.height - this.props.headerHeight
       const styles = {
