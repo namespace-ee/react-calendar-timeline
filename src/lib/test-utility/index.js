ca13499d3eb89ed0ca72c7d768aa48f334a5f0b9
Mike Joyce <michael.joyce@target.com>
2018-01-28 21:46:11 -0600

    prevent bubble of mousedown on header

--- src/lib/test-utility/index.js
+++ src/lib/test-utility/index.js
@@ -3,3 +3,9 @@ import { JSDOM } from 'jsdom'
 export function buildDom(domString) {
   return new JSDOM(`<!DOCTYPE html><body>${domString}></body>`)
 }
+
+export function sel(selectorString) {
+  return `[data-test-id="${selectorString}"]`
+}
+
+export function noop() {}
