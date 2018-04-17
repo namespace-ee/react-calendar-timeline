9f2b93bfcd22d5e4d8a02a7acc5db630ff07de12
Mike Joyce <michael.joyce@target.com>
2018-04-17 13:41:42 -0500

    removed preventDefault from handleDoubleClick item handler

--- src/lib/items/Item.js
+++ src/lib/items/Item.js
@@ -464,7 +464,6 @@ export default class Item extends Component {
   }
 
   handleDoubleClick = e => {
-    e.preventDefault()
     e.stopPropagation()
     if (this.props.onItemDoubleClick) {
       this.props.onItemDoubleClick(this.itemId, e)
