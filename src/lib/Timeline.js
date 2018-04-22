16f150418ec2efcd4727e72d9ee66f06286e61de
Penn Su <pennsu@gmail.com>
2018-04-21 23:23:40 -0700

    Only grab/update items within visibleTimeStart and visibleTimeEnd

--- src/lib/Timeline.js
+++ src/lib/Timeline.js
@@ -1082,8 +1082,8 @@ export default class ReactCalendarTimeline extends Component {
 
     const visibleItems = getVisibleItems(
       items,
-      canvasTimeStart,
-      canvasTimeEnd,
+      visibleTimeStart,
+      visibleTimeEnd,
       keys
     )
     const groupOrders = getGroupOrders(groups, keys)
