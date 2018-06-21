3b4d3b6f5c052e09dd5701f9e263325e555cdc75
Mike Joyce <michael.joyce@target.com>
2018-06-21 12:01:32 -0600

    removed comment

--- src/lib/markers/implementations/TodayMarker.js
+++ src/lib/markers/implementations/TodayMarker.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-// this should expect a timelineContext prop or some
-// way to calculate left position
-
 const criticalStyles = {
   position: 'absolute',
   top: 0,
