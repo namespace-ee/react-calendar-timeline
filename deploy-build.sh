202a499479b2a314539f1783cf4c842303cf3ee9
ilaiwi <ahmad.ilaiwi@gmail.com>
2018-06-25 13:41:48 +0300

    build

--- deploy-build.sh
+++ deploy-build.sh
@@ -11,16 +11,16 @@ function deploy_branch() {
   git clone --depth=1 --branch "$branch_name" "https://github.com/FoothillSolutions/react-calendar-timeline" ./$branch_name
 
   rm -rf ./$branch_name/*
-  cp -rf ./package.json ./README.md ./LICENSE ./$branch_name/* ./$branch_name
+  cp -rf ./package.json ./README.md ./LICENSE ./lib ./$branch_name
 
   cd ./$branch_name
 
   git add --all
   git commit -m "deploy($branch_name): $TRAVIS_COMMIT"
-  git push -qf "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
+  git push -qf "https://$GH_TOKEN@githZub.com/$TRAVIS_REPO_SLUG.git"
 
   cd ..
-  
+
 }
 
 deploy_branch "dist"
