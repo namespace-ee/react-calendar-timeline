a3c4235c1d8f61f12b4847f05a467c797a9c9ecd
Ahmad Ilaiwi <ahmad.ilaiwi@gmail.com>
2018-06-25 12:48:37 +0300

    build

---
+++ deploy-build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+yarn run build
+
+git config --global user.name ilaiwi
+git config --global user.email ahmad.ilaiwi@gmail.com
+
+function deploy_branch() {
+  local branch_name=$1
+
+  git clone --depth=1 --branch "$branch_name" "https://github.com/$TRAVIS_REPO_SLUG.git" ./$branch_name
+
+  rm -rf ./$branch_name/*
+  cp -rf ./package.json ./README.md ./LICENSE ./ramda/$branch_name/* ./$branch_name
+
+  cd ./$branch_name
+
+  git add --all
+  git commit -m "deploy($branch_name): $TRAVIS_COMMIT"
+  git push -qf "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
+
+  cd ..
+}
+
+deploy_branch "lib"
