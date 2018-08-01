7f21fc2fd748fc674e8380716e07e595adb4808d
Mike Joyce <michael.joyce@target.com>
2018-07-31 20:11:11 -0500

    remove webpack config related to prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,17 @@
-const webpack = require('webpack')
 const path = require('path')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-const nodeEnv = process.env.NODE_ENV || 'development'
 const port = process.env.PORT || 8888
 
-const isProd = nodeEnv === 'production'
-
 const config = {
-  devtool: isProd ? 'hidden-source-map' : 'cheap-eval-source-map',
+  devtool: 'cheap-eval-source-map',
   context: path.join(__dirname, './demo'),
   entry: {
     vendor: ['react', 'react-dom', 'faker', 'interactjs', 'moment'],
-    demo: isProd
-      ? ['./index.js']
-      : [
-          `webpack-dev-server/client?http://0.0.0.0:${port}`,
-          'webpack/hot/only-dev-server',
-          './index.js'
-        ]
+    demo: [
+      `webpack-dev-server/client?http://0.0.0.0:${port}`,
+      'webpack/hot/only-dev-server',
+      './index.js'
+    ]
   },
   output: {
     path: path.join(__dirname, './build'),
@@ -30,15 +23,11 @@ const config = {
     loaders: [
       {
         test: /\.css$/,
-        loader: isProd
-          ? ExtractTextPlugin.extract('style-loader', 'css-loader')
-          : 'style!css'
+        loader: 'style!css'
       },
       {
         test: /\.scss$/,
-        loader: isProd
-          ? ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
-          : 'style!css!sass'
+        loader: 'style!css!sass'
       },
       {
         test: /\.(html|png|jpg|gif|jpeg|svg)$/,
@@ -66,25 +55,6 @@ const config = {
       )
     }
   },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
-    new ExtractTextPlugin('[name].css'),
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(nodeEnv)
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      },
-      output: {
-        comments: false
-      },
-      sourceMap: false
-    })
-  ],
   devServer: {
     contentBase: './demo',
     port
