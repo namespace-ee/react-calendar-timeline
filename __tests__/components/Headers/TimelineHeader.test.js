d6d9cee66cca6c56dcaf6d49c1c286d7038489d2
sh-alawneh <s.alawneh@foothillsolutions.com>
2019-01-05 23:42:53 +0200

    adding test cases for the timeline wrapper's child components api

--- __tests__/components/Headers/TimelineHeader.test.js
+++ __tests__/components/Headers/TimelineHeader.test.js
@@ -23,6 +23,9 @@ const defaultProps = {
 
 describe('TimelineHeader', () => {
   afterEach(cleanup)
+  /**
+   * Testing The Default Functionality
+   */
   describe('renders default headers correctly', () => {
     it('renders two dateHeaders by default', () => {
       const { getAllByTestId, getByTestId } = renderDefaultTimeline()
@@ -33,6 +36,41 @@ describe('TimelineHeader', () => {
     it('renders two default sidebar headers if rightSidebarWidth is passed', () => {})
     it('renders two dateHeaders one primary and one secondary', () => {})
   })
+
+  /**
+   * Testing The Compoenets Each One By Its Own
+   */
+
+   describe("Testing SidebarHeader Component", () => {
+     it("passing no variant prop will render it above the left sidebar", () => {})
+     it("passing variant = left prop will render it above the left sidebar", () => {})
+     it("passing variant = right prop will render it above the right sidebar", () => {})
+   })
+
+   describe("Testing DateHeader Component", () => {
+     // Testing The Example In The Docs
+     it("DateHeader Will Be Rendered Correctly In The TimeLine", () => {})
+
+     it("providing a unit will reflect correctly to the date header", () => {})
+     it("setting primaryHeader to true will override the secondaryHeader value", () => {})
+     it("setting primaryHeader to true will override the secondaryHeader value", () => {})
+     it("setting primaryHeader to true will override the secondaryHeader value", () => {})
+     it("Label Format With A Function Will Rendered Correctly In The intervalRenderer", () => {})
+     it("Label Format With An Object Will Rendered Correctly In The intervalRenderer", () => {})
+     it("Label Format With A String Will Rendered Correctly In The intervalRenderer", () => {})
+   })
+
+   describe("Testing ItemHeader Component", () => {
+     // Testing The Example In The Docs
+     it("ItemHeader Will Be Rendered Correctly In The TimeLine", () => {})
+   })
+
+   // TODO: Writing Test Cases For The Custom Headers
+
+
+  /**
+   * Testing The Example Provided In The Docs
+   */
   it('renders headers in TimelineHeaders correctly', () => {
     const { getByText, rerender, queryByText } = render(
       <Timeline
