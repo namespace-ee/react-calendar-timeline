908b8611af83faafdb98857266dbce291f71422c
sh-alawneh <s.alawneh@foothillsolutions.com>
2019-01-06 10:00:05 +0200

    adding a new Test Case to The SidebarHeader test collection

--- __tests__/components/Headers/TimelineHeader.test.js
+++ __tests__/components/Headers/TimelineHeader.test.js
@@ -42,6 +42,9 @@ describe('TimelineHeader', () => {
    */
 
    describe("Testing SidebarHeader Component", () => {
+    //  Testing The Example In The Docs
+     it("SidebarHeader Will Be Rendered Correctly In The Timeline", () => {})
+     
      it("passing no variant prop will render it above the left sidebar", () => {})
      it("passing variant = left prop will render it above the left sidebar", () => {})
      it("passing variant = right prop will render it above the right sidebar", () => {})
