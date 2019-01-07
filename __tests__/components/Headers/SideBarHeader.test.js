572b1f4a0741df17dd71d8e27863cb90d88cfc73
sh-alawneh <s.alawneh@foothillsolutions.com>
2019-01-07 14:04:17 +0200

    implement the test cases in the sidebarHeader test file

--- __tests__/components/Headers/SideBarHeader.test.js
+++ __tests__/components/Headers/SideBarHeader.test.js
@@ -1,12 +1,96 @@
-/**
-   * Testing The Compoenets Each One By Its Own
-   */
+import React from 'react'
+import { render, cleanup } from 'react-testing-library'
+import Timeline from 'lib/Timeline'
+import DateHeader from 'lib/headers/DateHeader'
+import SidebarHeader from 'lib/headers/SidebarHeader'
+import TimelineHeaders from 'lib/headers/TimelineHeaders'
+import 'jest-dom/extend-expect'
 
-  describe("Testing SidebarHeader Component", () => {
-    //  Testing The Example In The Docs
-     it("SidebarHeader Will Be Rendered Correctly In The Timeline", () => {})
+import moment from 'moment'
 
-     it("passing no variant prop will render it above the left sidebar", () => {})
-     it("passing variant = left prop will render it above the left sidebar", () => {})
-     it("passing variant = right prop will render it above the right sidebar", () => {})
-   })
+import { items, groups } from '../../../__fixtures__/itemsAndGroups'
+import {
+  visibleTimeStart,
+  visibleTimeEnd
+} from '../../../__fixtures__/stateAndProps'
+
+const defaultProps = {
+  groups,
+  items,
+  defaultTimeStart: moment('1995-12-25').add(-12, 'hour'),
+  defaultTimeEnd: moment('1995-12-25').add(12, 'hour')
+}
+
+describe("Testing SidebarHeader Component", () => {
+  afterEach(cleanup)
+  //  Testing The Example In The Docs
+  it("SidebarHeader Will Be Rendered Correctly In The Timeline", () => {
+    const { getByTestId } = render(
+      <Timeline
+        {...defaultProps}
+      >
+        <TimelineHeaders>
+          <SidebarHeader>
+            {({ getRootProps }) => {
+              return <div data-testid="leftSidebarHeader" {...getRootProps()}>Left</div>
+            }}
+          </SidebarHeader>
+          <SidebarHeader variant="right">
+            {({ getRootProps }) => {
+              return <div data-testid="rightSidebarHeader" {...getRootProps()}>Right</div>
+            }}
+          </SidebarHeader>
+          <DateHeader primaryHeader />
+          <DateHeader />
+        </TimelineHeaders>
+      </Timeline>
+    )
+
+    expect(getByTestId('leftSidebarHeader')).toBeInTheDocument()
+    expect(getByTestId('rightSidebarHeader')).toBeInTheDocument()
+
+    expect(getByTestId('leftSidebarHeader').nextElementSibling.getAttribute('data-testid')).toBe('headerContainer')
+    expect(getByTestId('rightSidebarHeader').previousElementSibling.getAttribute('data-testid')).toBe('headerContainer')
+  })
+
+  it("passing no variant prop will render it above the left sidebar", () => {
+    const { getByTestId } = renderSidebarHeaderWithVariant()
+    expect(getByTestId('sidebarHeader')).toBeInTheDocument()
+    expect(getByTestId('sidebarHeader').nextElementSibling.getAttribute('data-testid')).toBe('headerContainer')
+  })
+  it("passing variant = left prop will render it above the left sidebar", () => {
+    const { getByTestId } = renderSidebarHeaderWithVariant("left")
+    expect(getByTestId('sidebarHeader')).toBeInTheDocument()
+    expect(getByTestId('sidebarHeader').nextElementSibling.getAttribute('data-testid')).toBe('headerContainer')
+  })
+  it("passing variant = right prop will render it above the right sidebar", () => {
+    const { getByTestId } = renderSidebarHeaderWithVariant("right")
+    expect(getByTestId('sidebarHeader')).toBeInTheDocument()
+    expect(getByTestId('sidebarHeader').previousElementSibling.getAttribute('data-testid')).toBe('headerContainer')
+  })
+
+  it("passing unusual variant value will render it above the left sidebar by default", () => {
+    const { getByTestId } = renderSidebarHeaderWithVariant("")
+    expect(getByTestId('sidebarHeader')).toBeInTheDocument()
+    expect(getByTestId('sidebarHeader').nextElementSibling.getAttribute('data-testid')).toBe('headerContainer')
+  })
+
+})
+
+
+function renderSidebarHeaderWithVariant(variant = undefined) {
+  return render(<Timeline
+    {...defaultProps}
+  >
+    <TimelineHeaders>
+      <SidebarHeader variant={variant}>
+        {({ getRootProps }) => {
+          return <div data-testid="sidebarHeader" {...getRootProps()}>SidebarHeader</div>
+        }}
+      </SidebarHeader>
+      <DateHeader primaryHeader />
+      <DateHeader />
+    </TimelineHeaders>
+  </Timeline>
+  )
+}
