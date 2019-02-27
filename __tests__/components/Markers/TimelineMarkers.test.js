50b6c9980b44f09355b8d711eca1585ecf8a233c
Gaston THL <gaston.niglia@thlonline.com>
2019-02-27 16:51:28 -0300

    fix(TimelineMarkers): Unsubscribing hidden markers correctly

--- __tests__/components/Markers/TimelineMarkers.test.js
+++ __tests__/components/Markers/TimelineMarkers.test.js
@@ -3,6 +3,7 @@ import { render } from 'react-testing-library'
 import 'jest-dom/extend-expect'
 import TimelineMarkers from 'lib/markers/public/TimelineMarkers'
 import TodayMarker from 'lib/markers/public/TodayMarker'
+import CustomMarker from 'lib/markers/public/CustomMarker'
 import { RenderWrapper } from 'test-utility/marker-renderer'
 
 describe('TimelineMarkers', () => {
@@ -22,5 +23,28 @@ describe('TimelineMarkers', () => {
         <TimelineMarkers />
       </RenderWrapper>
     )
+  })
+
+  it('is unsubscribed on unmounting after passing new date then hide it', ()=>{
+    const defaultCustomMarkerTestId = 'default-customer-marker-id'
+    const { queryByTestId, rerender } = render(
+      <RenderWrapper>
+        <TimelineMarkers>
+          <CustomMarker date={1000} />
+        </TimelineMarkers>
+      </RenderWrapper>)
+
+    rerender(<RenderWrapper>
+      <TimelineMarkers>
+        <CustomMarker date={2000} />
+      </TimelineMarkers>
+    </RenderWrapper>)
+
+    rerender(<RenderWrapper>
+      <TimelineMarkers>
+      </TimelineMarkers>
+    </RenderWrapper>)
+
+    expect(queryByTestId(defaultCustomMarkerTestId)).not.toBeInTheDocument()
   })
 })
