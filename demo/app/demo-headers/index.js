2f4e974c379df62c8cf18969037392a154595187
ilaiwi <ahmad.ilaiwi@gmail.com>
2019-04-08 13:34:09 +0300

    pull changes from tests

--- demo/app/demo-headers/index.js
+++ demo/app/demo-headers/index.js
@@ -194,38 +194,28 @@ export default class App extends Component {
           rightSidebarContent={<div>Above The Right</div>}
         >
           <TimelineHeaders className="header-background">
-            <SidebarHeader>
-              {({ getRootProps }) => {
-                console.log('left')
-                return <div {...getRootProps()}>Left</div>
-              }}
-            </SidebarHeader>
-            <SidebarHeader variant="right">
-              {({ getRootProps }) => {
-                return <div {...getRootProps()}>Right</div>
-              }}
-            </SidebarHeader>
+            <SidebarHeader/>
             <DateHeader
               labelFormat={this.state.format ? 'd' : undefined}
-              primaryHeader
+              unit= "primaryHeader"
             />
-            <DateHeader style={{ height: 50 }} />
-            <CustomHeader unit="year" props={{ hey: 'you' }}>
+            <DateHeader height={50} />
+            <CustomHeader unit="year" headerData={{ hey: 'you' }}>
               {(
                 {
                   headerContext: { intervals },
                   getRootProps,
                   getIntervalProps,
-                  showPeriod
+                  showPeriod,
+                  data,
                 },
-                props
+                
               ) => {
-                console.log('props', props)
+                console.log('props', data)
                 return (
-                  <div {...getRootProps({ style: { height: 30 } })}>
+                  <div {...getRootProps()}>
                     {intervals.map(interval => {
                       const intervalStyle = {
-                        // height: 30,
                         lineHeight: '30px',
                         textAlign: 'center',
                         borderLeft: '1px solid black',
@@ -261,10 +251,9 @@ export default class App extends Component {
                 showPeriod
               }) => {
                 return (
-                  <div {...getRootProps({ style: { height: 30 } })}>
+                  <div {...getRootProps()}>
                     {intervals.map(interval => {
                       const intervalStyle = {
-                        // height: 30,
                         lineHeight: '30px',
                         textAlign: 'center',
                         borderLeft: '1px solid black',
@@ -300,7 +289,7 @@ export default class App extends Component {
                 showPeriod
               }) => {
                 return (
-                  <div {...getRootProps({ style: { height: 30 } })}>
+                  <div {...getRootProps()}>
                     {intervals.map(interval => {
                       const intervalStyle = {
                         lineHeight: '30px',
@@ -329,13 +318,13 @@ export default class App extends Component {
             <DateHeader
               unit="week"
               labelFormat="MM/DD"
-              style={{ height: 50 }}
-              props={{ hey: 'date header' }}
+              height={50}
+              headerData={{ hey: 'date header' }}
               intervalRenderer={(
-                { getIntervalProps, intervalContext },
-                props
+                { getIntervalProps, intervalContext, data },
+                
               ) => {
-                console.log('intervalRenderer props', props)
+                console.log('intervalRenderer props', data)
                 return (
                   <div {...getIntervalProps()}>
                     {intervalContext.intervalText}
@@ -347,9 +336,9 @@ export default class App extends Component {
               ? [
                   <DateHeader
                     labelFormat={this.state.format ? 'd' : undefined}
-                    primaryHeader
+                    unit = "primaryHeader"
                   />,
-                  <DateHeader style={{ height: 50 }} />
+                  <DateHeader height={50} />
                 ]
               : null}
           </TimelineHeaders>
