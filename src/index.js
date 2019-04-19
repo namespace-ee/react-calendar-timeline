662e196651c2e17c388779dabed976c1e7f833da
Wang Shaoyu <wsysuper@gmail.com>
2019-04-19 10:33:27 +0900

    fix chrome73 problem wheel event listeners

--- src/index.js
+++ src/index.js
@@ -7,4 +7,55 @@ export { default as TodayMarker } from './lib/markers/public/TodayMarker'
 export { default as CustomMarker } from './lib/markers/public/CustomMarker'
 export { default as CursorMarker } from './lib/markers/public/CursorMarker'
 
+const EVENTS_TO_MODIFY = [
+  'touchstart',
+  'touchmove',
+  'touchend',
+  'touchcancel',
+  'wheel'
+]
+
+const originalAddEventListener = document.addEventListener.bind()
+
+const originalRemoveEventListener = document.removeEventListener.bind()
+
+if (document) {
+  document.addEventListener = (type, listener, options, wantsUntrusted) => {
+    let modOptions = options
+    if (EVENTS_TO_MODIFY.includes(type)) {
+      if (typeof options === 'boolean') {
+        modOptions = {
+          capture: options,
+          passive: false
+        }
+      } else if (typeof options === 'object') {
+        modOptions = {
+          passive: false,
+          ...options
+        }
+      }
+    }
+
+    return originalAddEventListener(type, listener, modOptions, wantsUntrusted)
+  }
+
+  document.removeEventListener = (type, listener, options) => {
+    let modOptions = options
+    if (EVENTS_TO_MODIFY.includes(type)) {
+      if (typeof options === 'boolean') {
+        modOptions = {
+          capture: options,
+          passive: false
+        }
+      } else if (typeof options === 'object') {
+        modOptions = {
+          passive: false,
+          ...options
+        }
+      }
+    }
+    return originalRemoveEventListener(type, listener, modOptions)
+  }
+}
+
 export default Timeline
