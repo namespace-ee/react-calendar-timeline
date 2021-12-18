2adb10fc94ddf7fd892a6ccc4cf91f8e90557f2c
Henrik Fuchs <atheck@gmail.com>
2021-12-18 17:46:30 +0100

    add tests for onEndScroll, onEndTimeChange

--- __tests__/components/Timeline/Timeline.test.js
+++ __tests__/components/Timeline/Timeline.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import moment from 'moment'
 import { mount } from 'enzyme'
 import Timeline from 'lib/Timeline'
-import { noop } from 'test-utility'
+import { noop, sel } from 'test-utility'
 
 const defaultProps = {
   ...Timeline.defaultProps,
@@ -10,8 +10,8 @@ const defaultProps = {
   groups: []
 }
 
-xdescribe('Timeline', () => {
-  describe('initialiation', () => {
+describe('Timeline', () => {
+  describe('initialization', () => {
     it('sets the visibleTime properties to defaultTime props', () => {
       const defaultTimeStart = moment('2018-01-01')
       const defaultTimeEnd = moment('2018-03-01')
@@ -59,6 +59,49 @@ xdescribe('Timeline', () => {
         'You must provide either "defaultTimeStart" and "defaultTimeEnd" or "visibleTimeStart" and "visibleTimeEnd" to initialize the Timeline'
       )
       jest.restoreAllMocks()
+    })
+  })
+
+  describe('scrolling', () => {
+    const visibleTimeStart = moment('2018-01-01').valueOf()
+    const visibleTimeEnd = moment('2018-03-01').valueOf()
+    const mockOnEndTimeChange = jest.fn();
+
+    afterEach(() => {
+      mockOnEndTimeChange.mockReset()
+    })
+
+    it('calls onEndScroll', () => {
+      const props = {
+        ...defaultProps,
+        visibleTimeStart,
+        visibleTimeEnd,
+        onEndTimeChange: mockOnEndTimeChange
+      }
+      const wrapper = mount(<Timeline {...props} />)
+
+      const scrollElement = wrapper.find(sel('scroll-element'))
+      wrapper.instance().scrollComponent.scrollLeft = 200
+      scrollElement.simulate('scroll');
+      scrollElement.simulate('mouseUp');
+
+      expect(mockOnEndTimeChange).toHaveBeenCalledTimes(1);
+    })
+
+    it('does not call onEndScroll if not scrolled', () => {
+      const props = {
+        ...defaultProps,
+        visibleTimeStart,
+        visibleTimeEnd,
+        onEndTimeChange: mockOnEndTimeChange
+      }
+      const wrapper = mount(<Timeline {...props} />)
+
+      const scrollElement = wrapper.find(sel('scroll-element'))
+      scrollElement.simulate('scroll');
+      scrollElement.simulate('mouseUp');
+
+      expect(mockOnEndTimeChange).not.toHaveBeenCalled();
     })
   })
 })
